67d09e531e83d9ac7fd6f7be11264b209770626c
Emily Jiang <emijiang6@googlemail.com>
2023-11-15 18:46:33 +0000

    use MP RM 3.0

--- src/main/java/com/example/demo/DemoRestApplication.java
+++ src/main/java/com/example/demo/DemoRestApplication.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import org.eclipse.microprofile.auth.LoginConfig;
 
-import javax.annotation.security.DeclareRoles;
+import jakarta.annotation.security.DeclareRoles;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  *
